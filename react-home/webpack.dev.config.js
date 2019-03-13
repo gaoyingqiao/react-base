9a9e736d3f04e0f398754f15a57ae8bd50317822
gaoyingqiao <gaoyinqiao@sluan.com>
2019-03-13 15:10:42 +0800

    webpack dev config

--- react-home/webpack.dev.config.js
+++ react-home/webpack.dev.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   /* 入口 */
-  entry: path.join(__dirname, 'src/index.js'),
+  entry: [
+    'react-hot-loader/patch',
+    path.join(__dirname, 'src/index.js')
+  ],
   /* 输出到 dist 文件夹，输出名字为 bundle.js */
   output: {
     path: path.join(__dirname, './dist'),
@@ -18,5 +21,26 @@ module.exports = {
       use: ['babel-loader?cacheDirectory=true'],
       include: path.join(__dirname, 'src')
     }]
+  },
+  /**
+   * webpack-dev-server为webpack打包生成的资源文件提供Web服务
+   */
+  devServer: {
+    contentBase: path.join(__dirname, './dist'),
+    port: 8080,
+    historyApiFallback: true, // 任意的404响应都被替代为index.html
+    // proxy: {
+    //   "/api": "http://localhost:3000"
+    // }
+  },
+  resolve: {
+    /** 
+     * 设置别名
+     */
+    alias: {
+      pages: path.join(__dirname, 'src/pages'),
+      component: path.join(__dirname, 'src/component'),
+      router: path.join(__dirname, 'src/router')
+    }
   }
 };
